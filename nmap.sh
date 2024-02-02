3bc5e06b866620b03a32606481bcf553ce58d902
zhsh9 <78374754+zhsh9@users.noreply.github.com>
2024-02-02 16:41:36 +0800

    Update nmap.sh to collect info of both open and filtered ports

--- nmap.sh
+++ nmap.sh
@@ -8,7 +8,7 @@ mkdir -p nmap file server crypto vuln priv
 
 # nmap scan:
 # - port, version, script, os, udp
-PORT=$(sudo nmap -sT --min-rate 10000 -p- $IP -oA nmap/ports | grep open | awk -F '/' '{print $1}' | paste -sd ',')
+PORT=$(sudo nmap -sT --min-rate 10000 -p- $IP -oA nmap/ports | grep -E 'open|filtered' | awk -F '/' '{print $1}' | paste -sd ',')
 sudo nmap -sT -sV -sC -O -p $PORT $IP -oA nmap/detail
 sudo nmap --script=vuln -p $PORT $IP -oA nmap/vuln
 sudo nmap -sU --top-ports 20 $IP -oA nmap/udp
